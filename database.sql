7e2ef263174460f9755c9bcda867496549e0d878
Kris Visness <kvisness@hbm2.com>
2020-10-27 10:16:02 -0500

    set up .env file

--- database.sql
+++ database.sql
@@ -3,8 +3,11 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
-CREATE TABLE "user" (
+CREATE TABLE "user"(
     "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
+    "username" VARCHAR(100) UNIQUE NOT NULL,
+    "gender" VARCHAR(1000) NOT NULL,
+    "age" INTEGER NOT NULL DEFAULT 0,
     "password" VARCHAR (1000) NOT NULL
 );
+
