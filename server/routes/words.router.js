e5e7a4bd2578309b3687ad5749cdb57de8421a26
Kris Visness <kvisness@hbm2.com>
2020-10-27 22:18:50 -0500

    words router works but is it needed

--- server/routes/words.router.js
+++ server/routes/words.router.js
@@ -2,13 +2,13 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-router.post('/words', (req, res) => {
-    const wordToAdd = req.body;
+router.post('/', (req, res) => {
+    const { sight_word, audio} = req.body;
     const queryText = `INSERT INTO "words" ("sight_word", "audio") VALUES ($1, $2);`;
-    pool.query(queryText, wordToAdd)
+    pool.query(queryText, [sight_word, audio])
         .then(() => res.sendStatus(201))
         .catch((err) => {
-            console.log(err);
+            console.log("in words router POST", err)
             res.sendStatus(500)
         });
 });
@@ -18,10 +18,9 @@ router.get('/', (req, res) => {
     pool.query(queryText)
         .then((result) => res.send(result.rows))
         .catch((err) => {
-            console.log(err);
+            console.log("error in words router GET", err);
             res.sendStatus(500)
         });
-    res.send(req.words);
 });
 
 // router.post('/words', (req, res) => {
